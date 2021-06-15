e6f5fddb3b9a617b49cae1140aed7d2925adff3e
unknown <bdaseh@gmail.com>
2021-06-15 11:23:12 -0700

    architecting the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { Component } from "react";
+
 import './App.css';
 
-function App() {
+import { CardList } from "./components/card-list/card-list";
+
+class App extends Component {
+constructor(){
+  super();
+  this.state = {
+    monsters:[
+      {
+        name: "frankenstein",
+        id: "1"
+      },
+      {
+        name: "dracula",
+        id: "2"
+      },
+      {
+        name: "zombie",
+        id: "3"
+      }
+    ]
+  }
+}
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardList name="Yihua"><h1>HELLO</h1></CardList>
+    {this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
     </div>
   );
 }
-
+}
 export default App;
