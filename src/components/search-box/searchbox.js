2d862e4c400fb54456858445dc19b25db34ac7fd
unknown <bdaseh@gmail.com>
2021-06-24 17:35:36 -0700

    return

--- src/components/search-box/searchbox.js
+++ src/components/search-box/searchbox.js
@@ -4,5 +4,7 @@ import "./searchbox.css"
 
 
 export const SearchBox = ({ placeholder, handleChange }) => {
+    return(
     <input className="search" type="search" placeholder={placeholder} onChange={handleChange}/>
+    )
 }
