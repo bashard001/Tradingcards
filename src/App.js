5e2309f2f9d33c34d282b267a8df474615a64b8d
unknown <bdaseh@gmail.com>
2021-06-25 17:38:38 -0700

    finish styling home screen

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ componentDidMount(){
     const filteredMonsters = monsters.filter( monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
   return (
     <div className="App">
+      <h1> Monsters Rolodex </h1>
       <SearchBox placeholder="search monster"  handleChange={e => this.setState({searchField: e.target.value})}/>
       <CardList monsters={filteredMonsters} />
     
